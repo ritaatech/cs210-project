3fce12e5235efe4b3038f2279820ffd2a9b4c813
Rita <ritaatech@gmail.com>
2025-12-10 22:48:03 -0800

    treat patient func and patient removal after treatment/death

--- main.cpp
+++ main.cpp
@@ -17,6 +17,17 @@ vector<Patient> patientList;
 int userLives = 3;
 int nextId = 1;
 
+string triageLevel(int level) {
+    if (level == 1)
+        return "Critical";
+    else if (level == 2)
+        return "Urgent";
+    else if (level == 3)
+        return "Non-urgent";
+    else
+        return "Unspecified";
+}
+
 void addPatient() {
     Patient temp;
     cout<<"\n========== New Patient ==========\n";
@@ -53,18 +64,7 @@ void addPatient() {
 
     patientList.push_back(temp);
     cout <<"\nThe Patient is added to the system.\n";
-}
-
-string triageLevel(int level) {
-    if (level == 1)
-        return "Critical";
-    else if (level == 2)
-        return "Urgent";
-    else if (level == 3)
-        return "Non-urgent";
-    else
-        return "Unspecified";
-}
+};
 
 void spawnPatient() {
     static int nameIndexCount = 0;
@@ -94,6 +94,40 @@ void spawnPatient() {
 }
 
 
+bool isBetterChoice(const Patient &a, const Patient &b) {
+    if (a.level < b.level) return true;
+    if (a.level > b.level) return false;
+    return a.waitRounds > b.waitRounds;
+}
+
+void treatPatient() {
+    cout << "\n========== Treat Patient ==========\n";
+    cout << "\n ----- Treating Most Severe Patient w/ Highest Wait Time ----- \n";
+    int bestIndex = -1;
+
+    for (int i = 0; i < static_cast<int>(patientList.size()); i++) {
+        if (patientList[i].status == "Waiting") {
+            if (bestIndex == -1 || isBetterChoice(patientList[i], patientList[bestIndex])) {
+                bestIndex = i;
+        }
+    }
+}
+
+    if (bestIndex == -1) {
+        cout << "No patients to treat.\n";
+        return;
+    }
+
+    Patient &p = patientList[bestIndex];
+
+    cout << "You have treated: " << p.name << " !!"
+         << " (Triage: " << triageLevel(p.level)
+         << ", Reason: " << p.reason << ")\n";
+    
+    patientList.erase(patientList.begin() + bestIndex);
+}
+
+
 void patientConditions() {
     cout << "\n========== Patients' Conditions ==========\n";
 
@@ -103,10 +137,20 @@ void patientConditions() {
     }
 
     bool patientWorsened = false;
+    bool patientDied = false;
 
-    for (int i = 0; i < patientList.size(); i++) {
+    for (int i = 0; i < static_cast<int>(patientList.size()); i++) {
         if (patientList[i].status == "Waiting") {
             patientList[i].waitRounds++;
+
+            int worseLimit;
+            if (patientList[i].level == 3)
+                worseLimit = 3;
+            else if (patientList[i].level == 2)
+                worseLimit = 2;
+            else
+                worseLimit = 999;
+
                 if (patientList[i].waitRounds >= 2 && patientList[i].level > 1) {
                     int previousLevel = patientList[i].level;
                     patientList[i].level--;
@@ -118,10 +162,27 @@ void patientConditions() {
                         << triageLevel(previousLevel) << " -> "
                         << triageLevel(patientList[i].level) << endl;
                 }
+
+            if (patientList[i].level == 1 && patientList[i].waitRounds >= 3) {
+                patientList[i].status = "Deceased";
+                patientList[i].waitRounds = 0;
+                userLives--;
+                patientDied = true;
+
+                cout << "\nPatient " << patientList[i].name
+                    << " has died ...\n";
+                cout << "Lives remaining: " << userLives << "\n";
+            }
         }
     }
     if (!patientWorsened) {
         cout<< "\nNo patient condition worsened.\n";
+    }
+
+    for (int i =  static_cast<int>(patientList.size()) - 1; i >= 0; --i) {
+        if (patientList[i].status == "Deceased") {
+            patientList.erase(patientList.begin() + i);
+        }
     }
  }
 
@@ -149,10 +210,12 @@ void menu() {
     cout<<"\n==============================\n";
     cout<<"  ✙ Hospital Triage System ✙ ";
     cout<<"\n==============================\n";
+    cout<<"       Total Lives: " << userLives << "\n";
     cout<< "   Patients on waitlist: " << patientList.size() << "\n";
     cout << "-------------------------------\n";
     cout<<"\n 1. Add a new patient\n";
     cout<<"\n 2. Show patients on waiting list\n";
+    cout<<"\n 3. Treat Next Patient\n";
     cout<<"\n 0. Exit\n";
 }
 
@@ -178,6 +241,9 @@ int main() {
         else if (choice == 2) {
             waitingList();
         }
+        else if (choice == 3) {
+            treatPatient();
+        }
         else if (choice == 0) {
             cout << "\nSystem closed.\n";
         }
@@ -187,6 +253,12 @@ int main() {
 
         if (choice != 0) {
             patientConditions();
+        }
+
+        if (userLives <= 0) {
+            cout << "\n You're Fired. Too many patients died in your care..\n";
+            cout << "\nThe ER could not keep up with the emergencies.\n";
+            break;
         }
 
     } while (choice != 0);
